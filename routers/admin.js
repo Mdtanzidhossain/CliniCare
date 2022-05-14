2f3dc7149e41e712f0393734864fbffbde600408
OmiRahman <momi183090@bscse.uiu.ac.bd>
2022-05-14 14:16:07 +0600

    updating again

--- routers/admin.js
+++ routers/admin.js
@@ -14,3 +14,15 @@ const {
   postAddAmbulance,
   getAddAmbulance,
 } = require('../controllers/admin')
+router.get('/add-product', getAddProduct)
+router.get('/products', getProducts)
+router.post('/add-product', postAddProduct)
+router.get('/edit-product/:productId', getEditProduct)
+router.post('/edit-product', postEditProduct)
+router.post('/delete-product', postDeleteProduct)
+router.post('/add-driver', postAddDriver)
+router.get('/add-driver', getAddDriver)
+router.get('/add-ambulance', getAddAmbulance)
+router.post('/add-ambulance', postAddAmbulance)
+
+module.exports = router

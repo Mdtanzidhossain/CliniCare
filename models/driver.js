32fea4d17532a6f90e982d5c4a6e03a44c95364d
unknown <fshaon183067@bscse.uiu.ac.bdgit config --global user.name 'fshaon>
2022-05-14 14:04:29 +0600

    adding model files

---
+++ models/driver.js
@@ -0,0 +1,33 @@
+const mongodb = require('mongodb')
+const getDb = require('../utils/db').getDb
+
+class Driver {
+  constructor(name, imageUrl, description, phone, id) {
+    this.name = name
+    this.imageUrl = imageUrl
+    this.description = description
+    this.phone = phone
+    this._id = id ? new mongodb.ObjectId(id) : null
+  }
+
+  save() {
+    const db = getDb()
+    let dbOp
+    if (this._id) {
+      dbOp = db
+        .collection('drivers')
+        .updateOne({ _id: this._id }, { $set: this })
+    } else {
+      dbOp = db.collection('drivers').insertOne(this)
+    }
+    return dbOp
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
+
+module.exports = Driver
